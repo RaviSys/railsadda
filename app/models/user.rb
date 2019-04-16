3cff75cfb2d689e0e3198bb99e7ef6d372e8a120
RaviSys <rksingh@isystango.com>
2019-04-16 15:49:58 +0530

    added cancancan and roles to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  ROLES = %w[admin collaborator guest]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -7,6 +10,12 @@ class User < ApplicationRecord
   has_many :visits, class_name: "Ahoy::Visit"
   has_many :comments
   
+  ROLES.each do |role|
+    define_method "#{role}?" do 
+      self.role == "#{role}"
+    end
+  end
+
   def name
     "#{self.first_name} #{self.last_name}".strip
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_183649) do
+ActiveRecord::Schema.define(version: 2019_04_16_101913) do
 
   create_table "ahoy_events", force: :cascade do |t|
     t.integer "visit_id"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_183649) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
